c4b193090e5c68fe52f554190e74d9e80093128c
asamarka-625 <mix0x@mail.ru>
2025-10-28 10:34:08 +0300

    update test_text

--- test_text.py
+++ test_text.py
@@ -1,6 +1,13 @@
 from app.crud import sql_get_text_by_id
 
 
+async def test_def():
+    legislation_id = input("Введите id документа: ")
+    text = await sql_get_text_by_id(legislation_id)
+    print(text)
+
+
 if __name__ == "__main__":
     import asyncio
-    asyncio.run(sql_get_text_by_id())
+
+    asyncio.run(test_def())
